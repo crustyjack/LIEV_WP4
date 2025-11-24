c4f529872f77a510d20f132b79b0355e7f0bd34d
crustyjack <44294431+crustyjack@users.noreply.github.com>
2025-11-24 12:03:48 +0000

    importing of data works

--- LIEV_WP4_model.py
+++ LIEV_WP4_model.py
@@ -1,5 +1,11 @@
-import streamlit as st
+# streamlit run LIEV_WP4_model.py
+
 import background_code
+import gspread
+
+import streamlit as st
+
+from google.oauth2.service_account import Credentials
 
 bg = background_code.BackgroundCode()
 
@@ -7,7 +13,7 @@ st.title("⚡MSR model Amsterdam")
 st.write(
     "We hope this is useful for you."
 )
-MSR = st.selectbox(
+MSR_name = st.selectbox(
     "Which MSR would you like to view the model for?",
     ("Sporenburg", "Choice 2", "Choice 3"))
 
@@ -17,6 +23,28 @@ Accom_elect_perc = st.slider("What percentage of accomodation is fully electric?
 
 year = st.slider("What year would you like to model?", 2025, 2050, 2025)
 
-st.write("You are modelling ", MSR, " MSR", "with an fully electric home adoption rate of ", Accom_elect_perc, "%, in the year ", year, ".")
+st.write("You are modelling ", MSR_name, " MSR", "with an fully electric home adoption rate of ", Accom_elect_perc, "%, in the year ", year, ".")
 
-sheety = bg.load_Gsheets()
+
+# Section to load in all dataframes if they are not in cached storage.
+# Load sheet only if not already in session_state
+if "sheet" not in st.session_state:
+    st.session_state.sheet = bg.load_Gsheets()
+
+sheet = st.session_state.sheet
+
+# Load profiles dataframe only if not already in session_state
+if "df_profiles" not in st.session_state:
+    st.session_state.df_profiles = bg.get_sheet_dataframe("Profiles", sheet)
+
+df_profiles = st.session_state.df_profiles
+
+# Load MSR dataframe only if not already in session_state
+if "df_MSRs" not in st.session_state:
+    st.session_state.df_MSRs = bg.get_sheet_dataframe("MSR_summary", sheet)
+
+df_MSRs = st.session_state.df_MSRs
+
+df_output = bg.profile_creator(df_profiles, df_MSRs, MSR_name)
+
+st.dataframe(df_output)
--- background_code.py
+++ background_code.py
@@ -1,5 +1,11 @@
-import streamlit as st
+# Written by: Michael Jenks
+# Last update: 24/11/2025
+
 import gspread
+
+import streamlit as st
+import pandas as pd
+
 from google.oauth2.service_account import Credentials
 
 class BackgroundCode:
@@ -24,8 +30,6 @@ class BackgroundCode:
 
         return spreadsheet
 
-
-
     def get_sheet_dataframe(self, sheet_name, sheet):
         """Read a worksheet into a DataFrame."""
         try:
@@ -35,3 +39,10 @@ class BackgroundCode:
         except gspread.WorksheetNotFound:
             st.warning(f"Worksheet '{sheet_name}' not found.")
             return pd.DataFrame()
+        
+    def profile_creator(self, df_profiles, df_MSRs, MSR_name):
+        df_MSR_profile = pd.DataFrame()
+        df_MSR_profile["DATUM_TIJDSTIP_2024"] = df_profiles["DATUM_TIJDSTIP_2024"].copy()
+        df_MSR_profile["DATUM_TIJDSTIP_2023"] = df_profiles["DATUM_TIJDSTIP_2023"].copy()
+        df_MSR_profile["Woning [kW]"] = df_profiles["Woning_AZI"].copy()*df_MSRs[MSR_name]
+        return df_MSR_profile
